92e66dd2d87536473b527fd68d8b36b3db623a44
Sujan G <sujansuji2009@gmail.com>
2022-12-31 19:52:28 +0530

    added login and signup support

--- Main/script.js
+++ Main/script.js
@@ -7,23 +7,9 @@ document.querySelector('#search-btn').onclick = () => {
   navbar.classList.remove('active');
 }
 
-let shoppingCart = document.querySelector('.shopping-cart');
 
-document.querySelector('#cart-btn').onclick = () => {
-  shoppingCart.classList.toggle('active');
-  searchForm.classList.remove('active');
-  loginForm.classList.remove('active');
-  navbar.classList.remove('active');
-}
 
-let loginForm = document.querySelector('.login-form');
 
-document.querySelector('#login-btn').onclick = () => {
-  loginForm.classList.toggle('active');
-  searchForm.classList.remove('active');
-  shoppingCart.classList.remove('active');
-  navbar.classList.remove('active');
-}
 
 let navbar = document.querySelector('.navbar');
 
