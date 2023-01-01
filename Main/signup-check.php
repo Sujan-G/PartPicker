eb78aa55f8b70fbf10d7e1cd28ffc3bbd7789908
Thejaswi S <thejaswi4uns@gmail.com>
2023-01-01 15:33:00 +0530

    commit

--- Main/signup-check.php
+++ Main/signup-check.php
@@ -57,7 +57,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])
 			header("Location: signup.php?error=The username is taken try another&$user_data");
 	        exit();
 		}else {
-           $sql2 = "INSERT INTO users(user_name, password, email) VALUES('$uname', '$pass', '$email')";
+           $sql2 = "INSERT INTO users(user_name, password, email,token) VALUES('$uname', '$pass', '$email','$token')";
            $result2 = mysqli_query($conn, $sql2);
            if ($result2) {
            	 header("Location: signup.php?success=Your account has been created successfully");
