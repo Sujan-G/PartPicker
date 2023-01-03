3c9367fb4e23daf5c232537f5a9ada4abc42eb8e
Sujan G <sujansuji2009@gmail.com>
2023-01-04 01:54:43 +0530

    commit

--- Main/admin.php
+++ Main/admin.php
@@ -1,9 +1,8 @@
 <?php 
 session_start(); 
-if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+if (isset($_SESSION['id']) || isset($_SESSION['user_name'])) {
 
   ?>
-?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -15,9 +14,7 @@ Admin Panel
 <!--     Fonts and icons     -->
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
 
-<!-- Nucleo Icons -->
-<link href="./fonts/nucleo-icons.css" rel="stylesheet" />
-<link href="./fonts/nucleo-svg.css" rel="stylesheet" />
+
 
 <!-- Font Awesome Icons -->
 <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
