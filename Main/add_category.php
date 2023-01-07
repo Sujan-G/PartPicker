d4e1dbe7fbb3b3e0d6e5c84f0003c4a319c7b03c
Sujan G <sujansuji2009@gmail.com>
2023-01-07 15:30:19 +0530

    commit

--- Main/add_category.php
+++ Main/add_category.php
@@ -1,4 +1,3 @@
-<!-- Starting Php Session -->
 <?php
 session_start();
 if (isset($_SESSION['id']) || isset($_SESSION['user_name'])) {
@@ -10,23 +9,23 @@ if (isset($_SESSION['id']) || isset($_SESSION['user_name'])) {
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
     <title>
       Admin Panel
     </title>
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-
-
-
+    <!-- Nucleo Icons -->
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-
     <!-- CSS Files -->
-    <link id="pagestyle" href="./css/material-dashboard.css?v=3.0.4" rel="stylesheet" />
-
+    <link id="pagestyle" href=".\css\material-dashboard.css" rel="stylesheet" />
+  </head>
     <!-- Alerify JS -->
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css" />
@@ -51,7 +50,7 @@ if (isset($_SESSION['id']) || isset($_SESSION['user_name'])) {
     }
   </style>
 
-  <body class="g-sidenav-show  bg-gray-100">
+<body class="g-sidenav-show  bg-gray-200">
     <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
       <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
@@ -59,52 +58,40 @@ if (isset($_SESSION['id']) || isset($_SESSION['user_name'])) {
           <span class="ms-1 font-weight-bold text-white">Admin Panel</span>
         </a>
       </div>
-
-
       <hr class="horizontal light mt-0 mb-2">
-
-      <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+      <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
         <ul class="navbar-nav">
-
-
-
-
-
-
-
-
-
           <li class="nav-item">
-            <a class="nav-link text-white active bg-gradient-primary " href="add_category.php">
-
+            <a class="nav-link text-white active bg-gradient-primary" href="add_category.php">
               <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="material-icons opacity-10">dashboard</i>
+                <i class="material-icons opacity-10">add</i>
               </div>
-
               <span class="nav-link-text ms-1">Add Category</span>
             </a>
           </li>
-
+          <li class="nav-item">
+            <a class="nav-link text-white " href="../pages/tables.html">
+              <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                <i class="material-icons opacity-10">add</i>
+              </div>
+              <span class="nav-link-text ms-1">Add Item</span>
+            </a>
+          </li>
         </ul>
       </div>
-
       <div class="sidenav-footer position-absolute w-100 bottom-0 ">
         <div class="mx-3">
-          <a class="btn bg-gradient-primary mt-4 w-100" href="adminlogout.php" type="button">Log Out</a>
-        </div>
-
+        <a class="btn bg-gradient-primary mt-4 w-100" href="adminlogout.php" type="button">Log Out</a>        </div>
       </div>
-
     </aside>
-
-    <main class="main-content border-radius-lg ">
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
       <!-- Navbar -->
-      <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+      <!-- <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
         <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-        </div>
-
+        </div> -->
+<!-- 
       </div>
-      </nav>
+      </nav> -->
 
       <!-- End Navbar -->
 
--- Main/admin.php
+++ Main/admin.php
@@ -1,205 +1,158 @@
-<!-- Starting Php Session -->
-<?php 
-session_start(); 
+<?php
+session_start();
 if (isset($_SESSION['id']) || isset($_SESSION['user_name'])) {
 
-  ?>
-<!DOCTYPE html>
-<html lang="en">
+?>
+  <!DOCTYPE html>
+  <html lang="en">
+
   <head>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>
-Admin Panel
- </title>
-<!--     Fonts and icons     -->
-<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <title>
+      Admin Panel
+    </title>
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <!-- Nucleo Icons -->
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+    <!-- CSS Files -->
+    <link id="pagestyle" href=".\css\material-dashboard.css" rel="stylesheet" />
+  </head>
 
-
-
-<!-- Font Awesome Icons -->
-<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-
-<!-- Material Icons -->
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-
-<!-- CSS Files -->
-<link id="pagestyle" href="./css/material-dashboard.css?v=3.0.4" rel="stylesheet" />
-</head>
-<body class="g-sidenav-show  bg-gray-100">
-<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
-<div class="sidenav-header">
-    <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-    <a class="navbar-brand m-0" href="admin.php">
-     <span class="ms-1 font-weight-bold text-white">Admin Panel</span>
-    </a>
-  </div>
-
-
-  <hr class="horizontal light mt-0 mb-2">
-
-  <div class="collapse navbar-collapse  w-auto  " id="sidenav-collapse-main">
-    <ul class="navbar-nav">
-      
-
-      
-        
-
-    <li class="nav-item">
-  <a class="nav-link text-white active bg-gradient-primary" href="add_category.php">
-    
-      <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-        <i class="material-icons opacity-10">dashboard</i>
+  <body class="g-sidenav-show  bg-gray-200">
+    <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
+      <div class="sidenav-header">
+        <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+        <a class="navbar-brand m-0" href="admin.php">
+          <span class="ms-1 font-weight-bold text-white">Admin Panel</span>
+        </a>
       </div>
-    
-    <span class="nav-link-text ms-1 ">Dashboard</span>
-  </a>
-</li>
-   
-
-          
-  
-<li class="nav-item">
-  <a class="nav-link text-white" href="add_category.php">
-    
-      <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-        <i class="material-icons opacity-10">add</i>
+      <hr class="horizontal light mt-0 mb-2">
+      <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-white active bg-gradient-primary" href="add_category.php">
+              <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                <i class="material-icons opacity-10">add</i>
+              </div>
+              <span class="nav-link-text ms-1">Add Category</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white " href="../pages/tables.html">
+              <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                <i class="material-icons opacity-10">add</i>
+              </div>
+              <span class="nav-link-text ms-1">Add Item</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">
+              <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                <i class="material-icons opacity-10">receipt_long</i>
+              </div>
+              <span class="nav-link-text ms-1">Billing</span>
+            </a>
+          </li>
+        </ul>
       </div>
-    
-    <span class="nav-link-text ms-1 ">Add Category</span>
-  </a>
-</li>
-
-
-<li class="nav-item">
-  <a class="nav-link text-white" href="#">
-    
-      <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-        <i class="material-icons opacity-10">add</i>
+      <div class="sidenav-footer position-absolute w-100 bottom-0 ">
+        <div class="mx-3">
+          <a class="btn bg-gradient-primary mt-4 w-100" href="adminlogout.php" type="button">Log Out</a>
+        </div>
       </div>
-    
-    <span class="nav-link-text ms-1 ">Add Items</span>
-  </a>
-</li>
-
-
-
-  
-<li class="nav-item">
-  <a class="nav-link text-white " href="./billing.html">
-    
-      <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-        <i class="material-icons opacity-10">receipt_long</i>
+    </aside>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+      <!-- End Navbar -->
+      <div class="container-fluid py-4">
+        <h2 class="font-weight-bolder mb-0">General Statistics</h2>
       </div>
-    
-    <span class="nav-link-text ms-1">Billing</span>
-  </a>
-</li>
 
-  </ul>
-  </div>
-  
-  <div class="sidenav-footer position-absolute w-100 bottom-0 ">
-    <div class="mx-3">
-      <a class="btn bg-gradient-primary mt-4 w-100" href="adminlogout.php" type="button">Log Out</a>
-    </div>
-    
-  </div>
-  
-</aside>
+      <div class="row">
+        <div class="col-lg-5 col-sm-5 mt-sm-0 mt-4">
+          <div class="card  mb-2">
+            <div class="card-header p-3 pt-2">
+              <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-xl mt-n4 position-absolute">
+                <i class="material-icons opacity-10">weekend</i>
+              </div>
+              <div class="text-end pt-1">
+                <p class="text-sm mb-0 text-capitalize">Bookings</p>
+                <h4 class="mb-0">281</h4>
+              </div>
+            </div>
 
-      <main class="main-content border-radius-lg ">
-        <!-- Navbar -->
-    <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-      <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+            <hr class="dark horizontal my-0">
+            <div class="card-footer p-3">
+              <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than last week</p>
+            </div>
           </div>
-              
-      </div>
-</nav>
 
-<!-- End Navbar -->
+          <div class="card  mb-2">
+            <div class="card-header p-3 pt-2">
+              <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary shadow text-center border-radius-xl mt-n4 position-absolute">
+                <i class="material-icons opacity-10">leaderboard</i>
+              </div>
+              <div class="text-end pt-1">
+                <p class="text-sm mb-0 text-capitalize">Today's Users</p>
+                <h4 class="mb-0">2,300</h4>
+              </div>
+            </div>
 
-<div class="container-fluid py-4">
-  <h2 class="font-weight-bolder mb-0">General Statistics</h2>
-</div>
+            <hr class="dark horizontal my-0">
+            <div class="card-footer p-3">
+              <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than last month</p>
+            </div>
+          </div>
 
-  <div class="row">
-      <div class="col-lg-5 col-sm-5">
-        <div class="card  mb-2">
-  <div class="card-header p-3 pt-2">
-    <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-xl mt-n4 position-absolute">
-      <i class="material-icons opacity-10">weekend</i>
-    </div>
-    <div class="text-end pt-1">
-      <p class="text-sm mb-0 text-capitalize">Bookings</p>
-      <h4 class="mb-0">281</h4>
-    </div>
-  </div>
+        </div>
+        <div class="col-lg-5 col-sm-5 mt-sm-0 mt-4">
+          <div class="card  mb-2">
+            <div class="card-header p-3 pt-2 bg-transparent">
+              <div class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
+                <i class="material-icons opacity-10">store</i>
+              </div>
+              <div class="text-end pt-1">
+                <p class="text-sm mb-0 text-capitalize ">Revenue</p>
+                <h4 class="mb-0 ">34k</h4>
+              </div>
+            </div>
 
-  <hr class="dark horizontal my-0">
-  <div class="card-footer p-3">
-    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than last week</p>
-  </div>
-</div>
+            <hr class="horizontal my-0 dark">
+            <div class="card-footer p-3">
+              <p class="mb-0 "><span class="text-success text-sm font-weight-bolder">+1% </span>than yesterday</p>
+            </div>
+          </div>
 
-        <div class="card  mb-2">
-  <div class="card-header p-3 pt-2">
-    <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary shadow text-center border-radius-xl mt-n4 position-absolute">
-      <i class="material-icons opacity-10">leaderboard</i>
-    </div>
-    <div class="text-end pt-1">
-      <p class="text-sm mb-0 text-capitalize">Today's Users</p>
-      <h4 class="mb-0">2,300</h4>
-    </div>
-  </div>
+          <div class="card ">
+            <div class="card-header p-3 pt-2 bg-transparent">
+              <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
+                <i class="material-icons opacity-10">person_add</i>
+              </div>
+              <div class="text-end pt-1">
+                <p class="text-sm mb-0 text-capitalize ">Followers</p>
+                <h4 class="mb-0 ">+91</h4>
+              </div>
+            </div>
 
-  <hr class="dark horizontal my-0">
-  <div class="card-footer p-3">
-    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than last month</p>
-  </div>
-</div>
-
-      </div>
-      <div class="col-lg-5 col-sm-5 mt-sm-0 mt-4">
-        <div class="card  mb-2">
-  <div class="card-header p-3 pt-2 bg-transparent">
-    <div class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
-      <i class="material-icons opacity-10">store</i>
-    </div>
-    <div class="text-end pt-1">
-      <p class="text-sm mb-0 text-capitalize ">Revenue</p>
-      <h4 class="mb-0 ">34k</h4>
-    </div>
-  </div>
-
-  <hr class="horizontal my-0 dark">
-  <div class="card-footer p-3">
-    <p class="mb-0 "><span class="text-success text-sm font-weight-bolder">+1% </span>than yesterday</p>
-  </div>
-</div>
-
-        <div class="card ">
-  <div class="card-header p-3 pt-2 bg-transparent">
-    <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
-      <i class="material-icons opacity-10">person_add</i>
-    </div>
-    <div class="text-end pt-1">
-      <p class="text-sm mb-0 text-capitalize ">Followers</p>
-      <h4 class="mb-0 ">+91</h4>
-    </div>
-  </div>
-
-  <hr class="horizontal my-0 dark">
-  <div class="card-footer p-3">
-    <p class="mb-0 ">Just updated</p>
-  </div>
-</div>
+            <hr class="horizontal my-0 dark">
+            <div class="card-footer p-3">
+              <p class="mb-0 ">Just updated</p>
+            </div>
+          </div>
   </body>
 <?php
-}else{
-     header("Location: login.php");
-     exit();
+} else {
+  header("Location: login.php");
+  exit();
 }
 ?>
-<!-- Ending php Session -->
-</html>
+
+  </html>
