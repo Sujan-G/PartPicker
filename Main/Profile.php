847627685ed77314f76b4f4ff81c35673c1f681a
Thejaswi S <thejaswi4uns@gmail.com>
2023-01-14 22:47:56 +0530

    profile changes

--- Main/Profile.php
+++ Main/Profile.php
@@ -1,43 +1,232 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
+include "db_conn.php";
+	if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+?>
+ <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Chivo+Mono&family=Inter&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="profile.css">
-    <script src="https://kit.fontawesome.com/0469953560.js" crossorigin="anonymous"></script>
-
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width , initial-scale=1">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Chivo+Mono&family=Inter&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="profile.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Chivo+Mono&family=Inter&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/0469953560.js" crossorigin="anonymous"></script>
+    </head>
 <title>Profile</title>
 
-<body>
-    <section class="header">
-        <nav>
-            <a href="home.php"><img src="image/partpicker.png"></a>
-            <div class="divlinks">
-                <li><a href="http://localhost:5500/login.html">Login</a></li>
-                <li><a href="http://localhost:5500/register.html">Register</a></li>
+<body>    
+    <header class="header">
+            <!-- making the logo link to the main home page -->
+            <a href="home.php" class="logo"> <img src="image\partpicker.png"></a>
+            <!-- creating a navbar -->
+            <nav class="navbar">
+                <a href="home.php">home</a>
+                <a href="builder.php">builder</a>
+                <!-- creating a drop down menu for all the products available -->
+                <div class="dropdown">
+                    <button class="dropbtn"><a>Browse Products <i class="fa-solid fa-caret-down"></i></button></a>
+                    <div class="dropdown-content">
+                        <table>
+                            <tr>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="cpu.php">
+                                            <img src="image/nav-cpu.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">CPU</div>
+                                    </div>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="memory.php">
+                                            <img src="image/nav-memory.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Memory</div>
+                                    </div>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="cpucooler.php">
+                                            <img src="image/nav-cpucooler.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">CPU Cooler</div>
+                                    </div>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="motherboard.php">
+                                            <img src="image/nav-motherboard.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Motherboard</div>
+                                    </div>
+                                <td>
+
+                            <tr>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="storage.php">
+                                            <img src="image/nav-ssd.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Storage</div>
+                                    </div>
+                                <td>
+
+                                    <div class="gallery">
+                                        <a target="_blank" href="videocard.php">
+                                            <img src="image/nav-videocard.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Graphics Card</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="powersupply.php">
+                                            <img src="image/nav-powersupply.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Power Supply</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="case.php">
+                                            <img src="image/nav-case.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Cabinet</div>
+                                    </div>
+                                </td>
+                        </table>
+                    </div>
+
+                </div>
+
+                <a href="#benchmarks">Benchmarks</a>
+            </nav>
+            <!-- Displaying User name at the top -->
+            <div class="login-status">
+
+                <a href="profile.php">
+
+                    <i class="fa-solid fa-user"> <?php echo "<styles>" . $_SESSION['user_name'];
+                                                    "</styles>" ?></i>
+                </a>
             </div>
-        </nav>
-    </section>
+            <!-- Creating icons for cart,searchbar and logout -->
+            <div class="icons">
+                <div class="fas fa-bars" id="menu-btn"></div>
+                <div class="fas fa-search" id="search-btn"></div>
+                <div class="fas fa-shopping-cart" id="cart-btn"></div>
+                <a href="logout.php">
+                    <div class="fa-solid fa-arrow-right-from-bracket" id="logout-btn"></div>
+                </a>
+            </div>
+            <form action="" class="search-form">
+                <input type="search" id="search-box" placeholder="search here...">
+                <label for="search-box" class="fas fa-search"></label>
+            </form>
+</header>
     <section>
         <hr>
         <h1>Profile</h1>
         <div class="links">
             <ul>
-                <li><a href=" "> Account </a></li>
-                <li><a href=" "> Comments </a></li>
-                <li><a href=" "> Build History </a></li>
+                <li><a href="profile.php"> Account </a></li>
+                <li><a href="bh.php"> Build History </a></li>
             </ul>
         </div>
-    </section>
-</body>
+        <div class="acc">
+        <form action="reset.php" method="post" enctype="multipart/form-data">
+            <?php
 
-</html>
+            $sql = "SELECT * FROM users WHERE id='{$_SESSION["id"]}'";
+            $result = mysqli_query($conn, $sql);
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+            ?>
+                    <input label for="Name">Name:</label>
+                    <input type="text" id="user_name" name="uname" placeholder="Name" value="<?php echo $_SESSION['user_name']; ?>" >
+                   
+                   <div class="Mail">
+                    <input label for="email">Email:</label>
+                    <input type="email" id="email" name="email" placeholder="Email" value="<?php echo $row['email'];?>" required>
+                    </div>
+                        <input label for="password">Password:</label>
+                        <input type="password" id="password" name="password" placeholder="Password" value="<?php echo $row['password']; ?>" required>
+            <?php
+                }
+            }
+            ?>
+            <br> 
+            <input type="submit" value="Reset Password">
+            </form>
+    </div>
+    </section>
+    <section class="footer" id="footer">
+            <table>
+                <tr>
+                    <td>
+                        <div class="f1">
+                            <img src="image/logo.png">
+                            <br>
+                            <p>
+                                PICK PARTS.<br>
+                                BUILD YOUR PC.<br>
+                                COMPARE AND SHARE.
+                            </p>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="f2">
+                            <center>
+                                <h1>Catergories</h1>
+                                <center>
+                                    <hr>
+                                    <br>
+                                    <center>
+                                        <p>
+                                            <a href="Builder.php">Builder</a><br>
+                                            <a href="Builder.php">Browse Products</a><br>
+                                            <a href="Builder.php">Benchmarks</a>
+                                        </p>
+                                    </center>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="f2">
+                            <center>
+                                <h1>Company</h1>
+                                <center>
+                                    <hr>
+                                    <br>
+                                    <center>
+                                        <p>
+                                            <a href="about.html">About</a><br>
+                                            <a href="contact.html">Contact Us</a><br>
+                                            <a href="#">User Code Of Conduct</a>
+                                        </p>
+                                    </center>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </section>
+        <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+        <!-- custom js file link  -->
+        <script src="script.js"></script>
+</body>
+<?php
+} else {
+    header("Location: login.php");
+    exit();
+}
+?>
+<!-- Ending Php session -->
+
+    </html>
--- Main/reset.php
+++ Main/reset.php
@@ -46,7 +46,7 @@ if ($token == " ") {
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>forget Password</title>
+	<title>forgot Password</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
