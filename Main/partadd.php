7fe15646c9a4d9b348f184670cf0295a35236313
Thejaswi S <thejaswi4uns@gmail.com>
2023-02-12 19:57:57 +0530

    c

--- Main/partadd.php
+++ Main/partadd.php
@@ -8,9 +8,13 @@ global $conn;
     $result = $conn->query($sql);
     $res = $result->fetch_assoc();
     $cat=$res['category_id'];
+    $sql = "SELECT price from products where id=$id";
+    $result = $conn->query($sql);
+    $res = $result->fetch_assoc();
+    $price=$res['price'];
     echo $cat;
-
-    $sql = "INSERT INTO builds(id,category_id) VALUES('$id','$cat')";
+ 
+    $sql = "INSERT INTO builds(id,category_id,price) VALUES('$id','$cat','$price')";
     $result = mysqli_query($conn, $sql);
     if ($result) {
         header("Location: builder.php?success=Your part has been added successfully");
