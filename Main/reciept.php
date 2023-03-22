1431327371051b5375e59b705dc0c25e360a26f0
Pranav BC <pranavbc.linkedin@gmail.com>
2023-03-22 16:12:28 +0530

    commit

---
+++ Main/reciept.php
@@ -0,0 +1,172 @@
+<?php
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width , initial-scale=1">
+        <link rel="stylesheet" href="parts.css">       
+         <link rel="stylesheet" href="reciept.css">
+         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Chivo+Mono&family=Inter&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/0469953560.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <title>CPUs</title>
+    </head>
+
+    <body>
+
+        <header class="header">
+
+            <a href="home.php" class="logo"> <img src="image\partpicker.png"></a>
+
+            <nav class="navbar">
+                <a href="home.php">home</a>
+                <a href="builder.php">builder</a>
+                <div class="dropdown">
+                    <button class="dropbtn"><a>Browse Products <i class="fa-solid fa-caret-down"></i></button></a>
+                    <div class="dropdown-content">
+                        <table>
+                            <tr>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="cpu.php">
+                                            <img src="image/nav-cpu.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">CPU</div>
+                                    </div>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="memory.php">
+                                            <img src="image/nav-memory.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Memory</div>
+                                    </div>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="cpucooler.php">
+                                            <img src="image/nav-cpucooler.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">CPU Cooler</div>
+                                    </div>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="motherboard.php">
+                                            <img src="image/nav-motherboard.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Motherboard</div>
+                                    </div>
+                                <td>
+
+
+                            <tr>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="storage.php">
+                                            <img src="image/nav-ssd.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Storage</div>
+                                    </div>
+                                <td>
+
+                                    <div class="gallery">
+                                        <a target="_blank" href="videocard.php">
+                                            <img src="image/nav-videocard.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Graphics Card</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="powersupply.php">
+                                            <img src="image/nav-powersupply.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Power Supply</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="gallery">
+                                        <a target="_blank" href="case.php">
+                                            <img src="image/nav-case.png" width="100" height="100">
+                                        </a>
+                                        <div class="desc">Cabinet</div>
+                                    </div>
+                                </td>
+                        </table>
+                    </div>
+                </div>
+
+
+                    <section class="footer" id="footer">
+            <table>
+                <tr>
+                    <td>
+                        <div class="f1">
+                            <img src="image/logo.png">
+                            <br>
+                            <p>
+                                PICK PARTS.<br>
+                                BUILD YOUR PC.<br>
+                                COMPARE AND SHARE.
+                            </p>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="f2">
+                            <center>
+                                <h1>Catergories</h1>
+                                <center>
+                                    <hr>
+                                    <br>
+                                    <center>
+                                        <p>
+                                            <a href="Builder.php">Builder</a><br>
+                                            <a href="browse.php">Browse Products</a><br>
+                                            <a href="Compare.php">Compare</a>
+                                        </p>
+                                    </center>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="f2">
+                            <center>
+                                <h1>Company</h1>
+                                <center>
+                                    <hr>
+                                    <br>
+                                    <center>
+                                        <p>
+                                        <a href="about.php">About</a><br>
+                                        <a href="ucc.php">User Code Of Conduct</a>
+                                        </p>
+                                    </center>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </section>
+        <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+        <!-- custom js file link  -->
+        <script src="script.js"></script>
+    </body>
+<?php
+} else {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+    </html>
